a70678a4116cb98b10b276e50aa6ee05a334b659
Malik Ebers <malik.ebers@web.de>
2020-03-10 22:11:47 +0100

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public Rigidbody Rb;
+
+	public float SideForce = 500f;
+	public float StraightForce = 2000f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rb.AddForce (0, 0, StraightForce * Time.deltaTime);
+		if (Input.GetKey("d")) {
+			Rb.AddForce (SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey("a")) {
+			Rb.AddForce (-SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+	}
+}
