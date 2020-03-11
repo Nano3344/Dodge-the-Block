a81cf2b528b19bc9661aaf4b887a580efff9b883
Malik Ebers <malik.ebers@web.de>
2020-03-11 22:20:20 +0100

    Added Score

---
+++ Assets/scripts/CollisionDetection.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour {
+
+	public Movement movement;
+
+	void OnCollisionEnter(Collision collisionInfo) {
+		if (collisionInfo.collider.tag == "Obstacle") {
+			movement.enabled = false;
+		}
+	}
+}
---
+++ Assets/scripts/TextChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class TextChange : MonoBehaviour {
+
+	public Transform player;
+	public Text Scoretext;
+	
+	// Update is called once per frame
+	void Update () {
+		Scoretext.text = player.position.z.ToString("0");
+	}
+}
